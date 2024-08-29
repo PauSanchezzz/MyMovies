a1f65050794af6fd34e8b8c93f2f918e318ea39a
PaulaSanchez <pausanchezt12@gmail.com>
2024-08-29 16:01:54 -0500

    create search bar

---
+++ components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <IconField class="search-bar" iconPosition="left">
+    <InputIcon class="pi pi-search"> </InputIcon>
+    <InputText class="input-search-bar" placeholder="¡Busca tú película favorita!" />
+  </IconField>
+</template>
+
+<style scoped>
+.input-search-bar {
+  width: 100%;
+  text-align: center;
+  font-size: 1.2rem;
+}
+.search-bar {
+  width: 100%;
+}
+</style>
