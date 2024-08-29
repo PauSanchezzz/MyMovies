cc9269b9ec1f52ba1c580b0c4954790b466ff852
PaulaSanchez <pausanchezt12@gmail.com>
2024-08-29 15:44:48 -0500

    create movie card

---
+++ components/MovieCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <Card style="width: 15rem; overflow: hidden">
+    <template #header>
+      <img alt="user header" class="movie-card-image" :src="props.image" />
+    </template>
+    <template #title>{{ props.title }}</template>
+    <template #subtitle>{{ props.subtitle }}</template>
+  </Card>
+</template>
+
+<style scoped>
+.movie-card-image {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+</style>
