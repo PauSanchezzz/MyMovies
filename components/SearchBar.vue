954be87b605e008e02441dbb9fc6130818fc1cf9
PaulaSanchez <pausanchezt12@gmail.com>
2024-08-29 16:16:39 -0500

    added button for search bar

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <IconField class="search-bar" iconPosition="left">
-    <InputIcon class="pi pi-search"> </InputIcon>
-    <InputText class="input-search-bar" placeholder="¡Busca tú película favorita!" />
-  </IconField>
+  <div class="search-bar">
+    <IconField class="input-search-bar" iconPosition="left">
+      <InputIcon class="pi pi-search"> </InputIcon>
+      <InputText class="text-search-bar" placeholder="¡Busca tú película favorita!" />
+    </IconField>
+    <Button severity="help" raised type="button" label="¡Buscar!" icon="pi pi-search" />
+  </div>
 </template>
 
 <style scoped>
-.input-search-bar {
+.text-search-bar {
   width: 100%;
   text-align: center;
   font-size: 1.2rem;
 }
+
+.input-search-bar {
+  width: 100%;
+}
+
 .search-bar {
   width: 100%;
+  display: flex;
+  gap: 1rem;
 }
 </style>
